c5fa43483259b3ee9159989714ba057fbe4fbf93
SpaceCat-Chan <49094338+SpaceCat-Chan@users.noreply.github.com>
2021-11-03 20:45:58 +0100

    update example to use rust bindings

--- examples/game_of_life/test.lua
+++ examples/game_of_life/test.lua
@@ -7,24 +7,17 @@ package.path = "../../../?.lua;"..package.path
 ]]
 
 local walp = require("walp.main")
+local bindings = require("walp.common_bindings.lua_impl")
 
 local module = walp.parse("test.wasm")
 
 -- .debug_info section is big, remove it before instantiation
 module.custom_sections = {}
 
-local memory = module.EXPORTS.memory
-
 math.randomseed(os.time())
 
 module.IMPORTS = {
     env = {
-        i_print = function(str_ptr, str_len)
-            for x=str_ptr, str_ptr+str_len-1 do
-                io.write(string.char(memory.read8(x)))
-            end
-        end,
-        i_random = math.random,
         print_u64 = function(n)
             print(n.l, n.h, n.l + n.h * 0x100000000)
         end,
@@ -37,6 +30,8 @@ module.IMPORTS = {
         end
     }
 }
+
+bindings(module)
 
 walp.instantiate(module)
 
