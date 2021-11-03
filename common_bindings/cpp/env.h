ba4aeb963197b212f77fc6c4ba4afec515ec06b9
William Katz <qualitycontentquestionmark@gmail.com>
2021-11-03 13:32:35 -0700

    :sparkles: `store_string()` and `print_ssi()`

--- common_bindings/cpp/env.h
+++ common_bindings/cpp/env.h
@@ -8,9 +8,9 @@ using SSI = int32_t;
 extern "C"
 {
 
-    void print(const char *str, int32_t size);
+    void print(const char *str_ptr, int32_t size);
     double math_random();
-    double tonumber(const char *str, int32_t size);
+    double tonumber(const char *str_ptr, int32_t size);
     SSI tostring(double number);
     int32_t get_string_len(SSI str_idx);
     bool write_string_to_ptr(SSI str_idx, char *str, int32_t size);
@@ -27,26 +27,28 @@ extern "C"
 
 static std::unordered_map<SSI, std::string> str_map;
 
-void print(const char *ptr, int32_t size) { printf("%.*s", size, ptr); }
+void print(const char *str_ptr, int32_t size) { printf("%.*s", size, str_ptr); }
 
 static std::random_device rd;
 static std::mt19937 gen(rd());
 static std::uniform_real_distribution<> dis(0.0, 1.0);
 double math_random() { return dis(gen); }
 
-double tonumber(char *str, int32_t size)
+double tonumber(const char *str_ptr, int32_t size)
 {
     double res;
-    strtod(str, (char **)&res);
+    strtod(str_ptr, (char **)&res);
     return res;
 }
+
 SSI tostring(double number)
 {
     std::string temp_str(std::to_string(number));
-    int32_t temp_hash = std::hash<std::string>{}(temp_str);
+    SSI temp_hash = std::hash<std::string>{}(temp_str);
     str_map.try_emplace(temp_hash, std::move(temp_str));
     return temp_hash;
 }
+
 int32_t get_string_len(SSI str_idx)
 {
     auto str_ref = str_map.find(str_idx);
@@ -59,6 +61,7 @@ int32_t get_string_len(SSI str_idx)
         return 0xFFFFFFFF;
     }
 }
+
 bool write_string_to_ptr(SSI str_idx, char *str, int32_t size)
 {
     auto str_ref = str_map.find(str_idx);
@@ -69,6 +72,17 @@ bool write_string_to_ptr(SSI str_idx, char *str, int32_t size)
     }
     return false;
 }
+
 void delete_string(SSI str_idx) { str_map.erase(str_idx); }
+
+SSI store_string(const char *str_ptr, int32_t size)
+{
+    std::string temp_str(std::string_view(str_ptr, size));
+    SSI temp_hash = std::hash<std::string>{}(str_ptr);
+    str_map.try_emplace(temp_hash, std::move(temp_str));
+    return temp_hash;
+}
+
+void print_ssi(SSI str_idx) { printf("%s", str_map.at(str_idx).data()); }
 
 #endif
